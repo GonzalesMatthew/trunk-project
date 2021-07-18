2a2c4ad1ef39e369c32295fb4d1db596cb95a378
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-07-17 22:31:43 -0500

    improved card styling

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from '@material-ui/core';
+import { makeStyles, Container, Paper } from '@material-ui/core';
 import StudentCard from '../components/StudentCard';
 import './App.scss';
 
 function App() {
+  // establish hooks to get students array
   const [students, setStudents] = useState([]);
-
   useEffect(() => {
     // eslint-disable-next-line no-undef
     fetch('https://api.hatchways.io/assessment/students')
@@ -19,23 +19,43 @@ function App() {
         console.error(error);
       });
   }, []);
+
   console.warn(students);
+
+  // make styles for the view
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      marginTop: 8,
+      marginBottom: 8,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      margin: 'auto',
+      maxWidth: 500,
+      backgroundColor: '#D1CBC1',
+      maxHeight: 380,
+      overflow: 'auto',
+    },
+  }));
+
+  const classes = useStyles;
+
   return (
-    <Container>
-      {students.map((student) => (
-        <StudentCard
-          key={student.id}
-          city={student.city}
-          company={student.company}
-          email={student.email}
-          firstName={student.firstName}
-          grades={student.grades}
-          id={student.id}
-          lastName={student.lastName}
-          pic={student.pic}
-          skill={student.skill}
-          />
-      ))}
+    <Container className={classes.container}>
+      <Paper className={classes.paper} elevation={6}>
+        {students.map((student) => (
+          <StudentCard
+            key={student.id}
+            company={student.company}
+            email={student.email}
+            firstName={student.firstName}
+            grades={student.grades}
+            lastName={student.lastName}
+            pic={student.pic}
+            skill={student.skill}
+            />
+        ))}
+      </Paper>
     </Container>
   );
 }
--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,40 +1,76 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {
-  Card, CardHeader, CardMedia, CardContent
+  makeStyles, Card, CardHeader, CardContent, Avatar,
 } from '@material-ui/core';
 
+// make styles for the view
+const useStyles = makeStyles((theme) => ({
+  container: {
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+    backgroundColor: '#D1CBC1',
+    maxHeight: 380,
+    overflow: 'auto',
+  },
+}));
+
 const StudentCard = ({
-  city,
   company,
   email,
   firstName,
   grades,
-  id,
   lastName,
   pic,
   skill,
-}) => (
-    <Card className="student-card">
-      <CardHeader
-        title={firstName}
-      />
-      <CardMedia
-        component='img'
-        alt= 'studentpic of'
-        image={pic}
-      />
-        <CardContent>{city} {company} {email} {grades} {id} {lastName} {skill} {lastName} </CardContent>
+}) => {
+  const classes = useStyles();
+  // compute average of grades array
+  let total = 0;
+  for (let i = 0; i < grades.length; i += 1) {
+    total += Number(grades[i]);
+  }
+  const average = `${total / grades.length}%`;
+  // end average computation
+
+  return (
+    <Card className="student-card d-flex flex-row'">
+      <div>
+        <Avatar
+          alt={`image of ${firstName} ${lastName}`}
+          src={pic}
+          className={classes.large}
+        />
+      </div>
+      <div className='d-flex flex-column flex-grow-1'>
+        <CardHeader
+          title={`${firstName} ${lastName}`}
+        />
+        <CardContent>
+          Email: {email}<br/>
+          Company: {company}<br/>
+          Skill: {skill}<br/>
+          Average:{average}
+        </CardContent>
+      </div>
     </Card>
-);
+  );
+};
 
 StudentCard.propTypes = {
-  city: PropTypes.string,
   company: PropTypes.string,
   email: PropTypes.string,
   firstName: PropTypes.string,
   grades: PropTypes.array,
-  id: PropTypes.string,
   lastName: PropTypes.string,
   pic: PropTypes.string,
   skill: PropTypes.string,
