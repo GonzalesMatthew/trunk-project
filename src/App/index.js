dea069294408705ae0ba27a0dbfaf8ba2320308d
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-07-17 19:46:51 -0500

    successfully getting students

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-import getStudents from '../helpers/data/UserData';
+// import getStudents from '../helpers/data/UserData';
 
 function App() {
   const [students, setStudents] = useState([]);
-  useEffect(() => {
-    getStudents().then(setStudents);
-  }, []);
 
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    fetch('https://api.hatchways.io/assessment/students')
+      .then((response) => response.json())
+      .then((jsonData) => {
+      // jsonData is parsed json object received from url
+        setStudents(jsonData);
+      })
+      .catch((error) => {
+      // handle your errors here
+        console.error(error);
+      });
+  }, []);
   console.warn(students);
 
   return (
--- src/helpers/data/UserData.js
+++ src/helpers/data/UserData.js
@@ -1,9 +1,15 @@
-import axios from 'axios';
-
-const getStudents = () => new Promise((resolve, reject) => {
-  axios.get('https://api.hatchways.io/assessment/students')
-    .then((response) => resolve(Object.values(response.data)))
-    .catch((error) => reject(error));
-});
+const getStudents = () => {
+  // eslint-disable-next-line no-undef
+  fetch('https://api.hatchways.io/assessment/students')
+    .then((response) => response.json())
+    .then((jsonData) => {
+    // jsonData is parsed json object received from url
+      console.warn(jsonData);
+    })
+    .catch((error) => {
+    // handle your errors here
+      console.error(error);
+    });
+};
 
 export default { getStudents };
