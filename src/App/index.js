bb2bbc54857ea21e1dacc3d5596b9ed93cf24823
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-07-18 15:35:52 -0500

    added styled-component Div to contain student cards and match prompt

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles, Container, Paper } from '@material-ui/core';
+import styled from 'styled-components';
 import StudentCard from '../components/StudentCard';
 import './App.scss';
 
@@ -11,52 +11,43 @@ function App() {
     fetch('https://api.hatchways.io/assessment/students')
       .then((response) => response.json())
       .then((jsonData) => {
-      // jsonData is parsed json object received from url
         setStudents(jsonData.students);
       })
       .catch((error) => {
-      // handle your errors here
         console.error(error);
       });
   }, []);
 
   console.warn(students);
 
-  // make styles for the view
-  const useStyles = makeStyles((theme) => ({
-    container: {
-      marginTop: 8,
-      marginBottom: 8,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      margin: 'auto',
-      maxWidth: 500,
-      backgroundColor: '#D1CBC1',
-      maxHeight: 380,
-      overflow: 'auto',
-    },
-  }));
-
-  const classes = useStyles;
+  // make styles for the view w styled-components
+  const Div = styled.div`
+    border-radius: 10px;
+    box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+    max-width: 1900px;
+    max-height: 600px;
+    overflow-y: auto;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `;
 
   return (
-    <Container className={classes.container}>
-      <Paper className={classes.paper} elevation={6}>
-        {students.map((student) => (
-          <StudentCard
-            key={student.id}
-            company={student.company}
-            email={student.email}
-            firstName={student.firstName}
-            grades={student.grades}
-            lastName={student.lastName}
-            pic={student.pic}
-            skill={student.skill}
-            />
-        ))}
-      </Paper>
-    </Container>
+    <Div>
+      {students.map((student) => (
+        <StudentCard
+          key={student.id}
+          company={student.company}
+          email={student.email}
+          firstName={student.firstName}
+          grades={student.grades}
+          lastName={student.lastName}
+          pic={student.pic}
+          skill={student.skill}
+          />
+      ))}
+    </Div>
   );
 }
 
